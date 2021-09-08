28123be47ceff6a7ab87ccd323ea89d9a536080b
jedigrandmaster777 <88267314+jedigrandmaster777@users.noreply.github.com>
2021-09-07 23:25:54 -0600

    added main javascript file

---
+++ lib.js
@@ -0,0 +1,253 @@
+var pjs = function(id){
+	if(typeof id === "string"){
+		this.canvas = document.getElementById(id);
+	} else if(typeof id === "object"){
+		this.canvas = id;
+	}
+	try{
+		this.ctx = this.canvas.getContext("2d");
+	} catch(error){
+		throw new Error("Supplied element is not a canvas");
+	}
+
+	this.ctx.fillStyle = "#FFFFFF";
+	
+	this.dostroke = true;
+	this.dofill = true;
+}
+
+pjs.createCanvas = function(){
+	var newCanvas = document.createElement("canvas");
+	newCanvas.height = 400;
+	newCanvas.width = 400;
+	newCanvas.style = "display: inline;border:1px solid #000000;";
+
+	document.body.appendChild(newCanvas);
+	return new pjs(newCanvas);
+}
+
+//drawing 
+pjs.prototype.ellipse = function(x, y, width, height){//should be optimized
+	if(this.dostroke === false && this.dofill === false){
+		console.log("Error: both stroke and fill are turned off");
+		return;
+	}
+	this.ctx.beginPath();
+	this.ctx.ellipse(x, y, width, height, Math.PI / 4, 0, 2 * Math.PI);
+	if(this.dofill){
+		this.ctx.fill();
+	}
+	if(this.dostroke){
+		this.ctx.stroke();
+	}
+}
+pjs.prototype.triangle = function(x1, y1, x2, y2, x3, y3){// should be optimized
+	if(this.dostroke === false && this.dofill === false){
+		console.log("Error: both stroke and fill are turned off");
+		return;
+	}
+	this.ctx.beginPath();
+	this.ctx.moveTo(x1, y1);
+	this.ctx.lineTo(x2, y2);
+	this.ctx.lineTo(x3, y3);
+	this.ctx.lineTo(x1, y1);
+	if(this.dofill){
+		this.ctx.fill();
+	}
+	if(this.dostroke){
+		this.ctx.stroke();
+	}
+}
+pjs.prototype.rect = function(x, y, width, height){
+	if(this.dostroke === false && this.dofill === false){
+		console.log("Error: both stroke and fill are turned off");
+		return;
+	}
+	if(this.dofill){
+		this.ctx.fillRect(x, y, width, height);
+	}
+	if(this.dostroke){
+		this.ctx.strokeRect(x, y, width, height);
+	}
+}
+pjs.prototype.line = function(x1, y1, x2, y2){//should be optimized
+	if(this.dostroke === false){
+		console.log("Error: stroke is turned off but you are attempting to draw a line");
+		return;
+	}
+	this.ctx.beginPath();
+	this.ctx.moveTo(x1, y1);
+	this.ctx.lineTo(x2, y2);
+	this.ctx.stroke();
+}
+pjs.prototype.background = function(r, g, b){
+	this.prevfill = this.ctx.fillStyle;
+	this.ctx.fillStyle = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);//confirm this works
+	this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width);
+	this.ctx.fillStyle = this.prevfill;
+}
+pjs.prototype.fill = function(r, g, b){
+	if(this.dofill !== true){
+		this.dofill = true; 
+	}
+	this.ctx.fillStyle = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);//confirm this works
+}
+pjs.prototype.stroke = function(r, g, b){
+	if(this.dostroke !== true){
+		this.dostroke = true;
+	}
+	this.ctx.strokeStyle = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);//confirm this works
+}
+pjs.prototype.strokeWeight = function(weight){
+	this.ctx.lineWidth = weight;
+}
+pjs.prototype.noStroke = function(){
+	this.dostroke = false;
+}
+pjs.prototype.noFill = function(){
+	this.dofill = false;
+}
+
+//utilities
+pjs.random = function(min, max){
+  min = Math.ceil(min);
+	max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+pjs.setCookie = function(cname, cvalue, exdays){
+ 	const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+pjs.getCookie = function(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+//mouse event listeners
+pjs.mouseX = 0;
+pjs.mouseY = 0;
+pjs.mouseMoved = function(){
+	throw new Error("mouseMoved is not defined");
+}
+pjs.mouseClicked = function(button){
+	throw new Error("mouseClicked is not defined");
+}
+pjs.mouseTemplate = pjs.mouseMoved;
+pjs.mouseClickedTemplate = pjs.mouseClicked;
+pjs.internalMouseListener = function(e){
+	if(pjs.mouseTemplate !== pjs.mouseMoved){ 
+		pjs.mouseMoved();
+	}
+	pjs.mouseX = e.clientX;
+	pjs.mouseY = e.clientY;	
+}
+document.addEventListener("mousemove", pjs.internalMouseListener);
+pjs.prototype.getMouseOffset = function(){
+	return {
+		x: pjs.mouseX - this.canvas.offsetLeft,
+		y: pjs.mouseY - this.canvas.offsetTop
+	};
+}
+//keyboard event listeners:
+pjs.keyPressed = function(e){
+	throw new Error("keyPressed has not been defined"); //maybe remove
+}
+pjs.keyReleased = function(e){
+	throw new Error("keyReleased has not been defined");
+}
+pjs.template = pjs.keyPressed;
+pjs.template2 = pjs.keyReleased;
+
+pjs.keys = [];
+
+pjs.internalKeyPressListener = function(e){
+	if(pjs.keyPressed !== pjs.template){
+		pjs.keyPressed(e);
+	}
+	if(pjs.keys[e.key] !== true){
+		pjs.keys[e.key] = true;
+	}
+}
+pjs.internalKeyReleaseListener = function(e){
+	if(pjs.keyReleased !== pjs.template2){
+		pjs.keyReleased(e);
+	}
+	if(pjs.keys[e.key] !== false){
+		pjs.keys[e.key] = false;
+	}
+}
+document.addEventListener("keydown", pjs.internalKeyPressListener);
+document.addEventListener("keyup", pjs.internalKeyReleaseListener);
+pjs.getKey = function(key){
+	if(pjs.keys[key] === true){
+		return true;
+	} else {
+		return false;
+	}
+}
+//event listeners
+
+pjs.draw = function(){
+	throw new Error("draw has not been defined");
+}
+
+//making event listeners work
+pjs.debug = false;
+pjs.template3 = pjs.draw;
+
+pjs.drawId = null;
+
+pjs.loadEventListenerIsActive = true;
+
+pjs.loadEventListeners = function(){
+	if(pjs.draw !== pjs.template3){
+		pjs.drawId = setInterval(pjs.draw, 33);
+		pjs.template3 = null;
+	} else {
+		if(pjs.debug){
+			console.log("draw has not been defined");
+		}
+	}
+	if(pjs.mouseClicked !== pjs.mouseClickedTemplate){
+		document.addEventListener("mousedown", pjs.mouseClicked);
+		pjs.mouseClickedTemplate = null;
+	} else {
+		if(pjs.debug){
+			console.log("mouseClicked has not been defined");
+		}
+	}
+	window.removeEventListener("load", pjs.loadEventListeners);
+	pjs.loadEventListenerIsActive = false;
+}
+
+window.addEventListener("load", pjs.loadEventListeners); //not the best way of doing things, but it works
+
+//update this at some point
+pjs.selfDestruct = function(){
+	if(pjs.drawId !== null){
+		clearInterval(pjs.drawId);
+	}
+	if(pjs.template === null){
+		document.removeEventListener("keypress", pjs.keyPressed);
+	}
+	if(pjs.template2 === null){
+		document.removeEventListener("keyup", pjs.keyReleased);
+	}
+	if(pjs.loadEventListenerIsActive === true){
+		window.removeEventListener("load", pjs.loadEventListeners);
+		pjs.loadEventListenerIsActive = false; //probably not necessary 
+	}
+	pjs = null;
+}
